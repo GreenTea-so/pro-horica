143983090473a1c0818586ce79b2ae6f08d36758
GreenTea-so <lavrensepatton@gmail.com>
2025-08-14 22:14:18 +0300

    4

--- src/app/components/YandexMap/index.jsx
+++ src/app/components/YandexMap/index.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState } from 'react';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import bem from "../../utils/bem";
 import './index.scss';
@@ -7,15 +8,22 @@ import './index.scss';
 const b = bem('yandex-map');
 
 export default function YandexMap() {
+  
+  const [isClick, setIsClick] = useState(false);
+
+  const handleClick = () => {
+    setIsClick(true);
+  }
+
   return (
     <div className="my-5">
       <span className="title mb-4">Наш выставочный зал в Красногорске</span>
       <YMaps>
-        <div>
-          <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} height={550}>
-            <Placemark geometry={[55.814750, 37.362711]} />
-          </Map>
-        </div>
+        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} height={550}>
+        <div className={b('vual', { click: isClick })} onClick={handleClick} />
+
+          <Placemark geometry={[55.814750, 37.362711]} />
+        </Map>
       </YMaps>
     </div>
   );
