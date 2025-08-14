d58ae062d46a66feca97c20d874cabd83969f162
GreenTea-so <lavrensepatton@gmail.com>
2025-08-14 22:22:55 +0300

    2

--- src/app/components/YandexMap/index.jsx
+++ src/app/components/YandexMap/index.jsx
@@ -19,7 +19,7 @@ export default function YandexMap() {
     <div className="my-5">
       <span className="title mb-4">Наш выставочный зал в Красногорске</span>
       <YMaps>
-        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} height={550}>
+        <Map id="map" defaultState={{ center: [55.75, 37.57], zoom: 9 }} height={550}>
         <div className={b('vual', { click: isClick })} onClick={handleClick} />
 
           <Placemark geometry={[55.814750, 37.362711]} />
